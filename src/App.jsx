4b4907a55eda67bdb93ac15ee18d65ae9b77f568
Tanvir Memon <tanvirmemon48@gmail.com>
2024-12-04 19:21:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./App.css";
+function App() {
+  return (
+    <div className="main-card">
+    <div className="container ">
+      
+      <div className="row">
+        <div className="col-md-2">
+          <div className="img">     <img src="1.png" alt="" /></div>
+   
+        </div>
+        <div className="col-md-2">
+          <div className="img">     <img src="2.png" alt="" /></div>
+   
+        </div>
+        <div className="col-md-2">
+          <div className="img">     <img src="3.png" alt="" /></div>
+   
+        </div>
+        <div className="col-md-2">
+          <div className="img">     <img src="4.png" alt="" /></div>
+   
+        </div>
+        <div className="col-md-2">
+          <div className="img">     <img src="5.png" alt="" /></div>
+   
+        </div>
+        <div className="col-md-2">
+          <div className="img">     <img src="6.png" alt="" /></div>
+   
+        </div>
+        </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
+
