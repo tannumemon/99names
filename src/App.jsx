941ee8a5a949cc00c385201f1926e08213961165
Tanvir Memon <tanvirmemon48@gmail.com>
2024-12-04 19:52:50 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,34 @@ function App() {
     <div className="container ">
       
       <div className="row">
-        <div className="col-md-2">
-          <div className="img">     <img src="1.png" alt="" /></div>
-   
+              <div className="col-md-4">
+                <div className="img">     <img src="1.png" alt="" /></div>
+        
+              </div>
+              <div className="col-md-4">
+                <div className="img">     <img src="2.png" alt="" /></div>
+        
+              </div>
+              <div className="col-md-4">
+                <div className="img">     <img src="3.png" alt="" /></div>
+        
+              </div>
+              
         </div>
-        <div className="col-md-2">
-          <div className="img">     <img src="2.png" alt="" /></div>
-   
-        </div>
-        <div className="col-md-2">
-          <div className="img">     <img src="3.png" alt="" /></div>
-   
-        </div>
-        <div className="col-md-2">
-          <div className="img">     <img src="4.png" alt="" /></div>
-   
-        </div>
-        <div className="col-md-2">
-          <div className="img">     <img src="5.png" alt="" /></div>
-   
-        </div>
-        <div className="col-md-2">
-          <div className="img">     <img src="6.png" alt="" /></div>
-   
-        </div>
-        </div>
+            <div className="row">
+            <div className="col-lg-4">
+                    <div className="img">     <img src="4.png" alt="" /></div>
+            
+                  </div>
+                  <div className="col-lg-4">
+                    <div className="img">     <img src="5.png" alt="" /></div>
+            
+                  </div>
+                  <div className="col-lg-4">
+                    <div className="img">     <img src="6.png" alt="" /></div>
+            
+                  </div>
+            </div>
     </div>
     </div>
   );
